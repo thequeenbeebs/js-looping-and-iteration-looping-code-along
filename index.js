9994e41b56cba762c11f2c5caa48b973714f0d6a
Blaire Baker <bakerblaire@gmail.com>
2021-01-09 15:00:13 -0600

    commit tree 3752ec9b19417de5d4be7e73d63ee89f93b76934

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    let thankYou = [];
+    for (let i = 0; i < array.length; i++) {
+        thankYou.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYou;
+}
+
+function countDown(integer) {
+    while (integer > -1) {
+        console.log(integer);
+        integer--;
+    }
+}
